423bf91fd8f79478cea18870eb78e0deb9c9f2b8
YASH SINHA <74976948+yashsinha0206@users.noreply.github.com>
2022-05-28 00:30:03 +0530

    Update db.connection.js

--- services/db.connection.js
+++ services/db.connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const env = require('../env')
 
-var inst1 = mongoose.createConnection(`mongodb://localhost:27017/engage`)
+var inst1 = mongoose.createConnection(process.env.MONGODB_URL)
 module.exports.cars_db = inst1
 
